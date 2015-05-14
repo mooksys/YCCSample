7488ee4b08a705972b5b0b48cbf39cc0a7427a14
SEO JAE WON <cinos81@gmail.com>
2015-05-14 14:46:37 +0900

    9장 완료

---
+++ 08/Sample5/Sample5/Sample5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int* pA;
+	
+    a = 5;
+    pA = &a;
+	
+    cout << "변수 a의 값은 " << a << " 입니다.\n";
+
+    *pA = 50;
+
+    cout << "*pA에50을 대입했습니다.\n";
+    cout << "변수 a의 값은 " << a << " 입니다.\n";
+
+    return 0;
+}
