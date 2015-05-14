15219d57ee0043db7d866b6080ed727193fb8397
SEO JAE WON <cinos81@gmail.com>
2015-05-14 14:01:51 +0900

    5장 완료

---
+++ 05/Sample4/Sample4/Sample4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int res;
+
+    cout << "정수를 입력하십시오.\n";
+
+    cin >> res;
+
+    if (res == 1){
+        cout << "1이 입력 되었습니다.\n";
+    }
+    else if (res == 2){
+        cout << "2가 입력 되었습니다.\n";
+    }
+    else{
+        cout << "1 또는 2를 입력하십시오.\n";
+    }
+
+    return 0;
+}
