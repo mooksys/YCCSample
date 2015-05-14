63846f29e9bfae3221bf5828c5c4697b33ea982f
SEO JAE WON <cinos81@gmail.com>
2015-05-14 16:51:56 +0900

    16장 완료

---
+++ 16/Sample8/Sample8/Sample8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "10을 10진수로 표기하면 " << dec << 10 << " 입니다.\n";
+    cout << "10을 8진수로 표기하면 " << dec << 10 << " 입니다.\n";
+    cout << "12를 8진수로 표기하면 " << dec << 12 << " 입니다.\n";
+    cout << "10을 16진수로 표기하면 " << dec << 10 << " 입니다.\n";
+
+    return 0;
+}
